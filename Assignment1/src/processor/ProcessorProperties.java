0793703a4abe0f9216a40f89623cb2ab585e091b
Shoaib-Ahmed <sahmed25@asu.edu>
2012-09-09 19:11:04 -0700

    JOTD Implementation
    TCP Client implementation to retrive JOTD has been checked in. A string
    with the JOTD will be returned.

--- Assignment1/src/processor/ProcessorProperties.java
+++ Assignment1/src/processor/ProcessorProperties.java
@@ -6,24 +6,49 @@ package processor;
  *
  */
 
-public class ProcessorProperties {
+public class ProcessorProperties {	
 
 	/** 
-	 * Static properties pointing to the URL to get location from IP. Also included Tags to parse the returned information (XML is being returned).
+	 * Enumeration against Message Types.
 	 */
-	public static String LocationFromIPGetURL = "http://services.ipaddresslabs.com/iplocation/locateip?key=demo&ip=";
-	public static String LocationFromIPStateElement = "<region_name>";
-	public static String LocationFromIPStateElementEnd = "</region_name>";
-	public static String LocationFromIPCityElement = "<city>";
-	public static String LocationFromIPCityElementEnd = "</city>";
+	public enum MessageType
+	{ HELLO , HI , GOODBYE , REQUEST , RESPONSE , NONE ;
+		public static MessageType GetMessageType(String Str)
+		{
+			try {return valueOf(Str);}
+			catch (Exception ex){return NONE;}
+		}
+	};
 	
 	/** 
-	 * Static properties pointing to the URL to get location from Coordinate. Also included Tags to parse the returned information (XML is being returned).
+	 * Static properties pointing to the URL to get location from IP. Also include Tags to parse the returned information (XML is being returned).
+	 */	
+	public static String LocationFromIPCityElement = "<city>";
+	public static String LocationFromIPCityElementEnd = "</city>";	
+	public static String LocationFromIPStateElement = "<region_name>";
+	public static String LocationFromIPStateElementEnd = "</region_name>";
+	public static String LocationFromIPGetURL = "http://services.ipaddresslabs.com/iplocation/locateip?key=demo&ip=";
+	
+	/** 
+	 * Static properties pointing to the URL to get location from Coordinate. Also include Tags to parse the returned information (XML is being returned).
 	 */
-	public static String LocationFromCoordinateGetURL = "http://where.yahooapis.com/geocode?q=";
 	public static String LocationFromCoordinateGetURL2 = "&gflags=R";
-	public static String LocationFromCoordinateStateElement = "<state>";
-	public static String LocationFromCoordinateStateElementEnd = "</state>";
 	public static String LocationFromCoordinateCityElement = "<city>";
+	public static String LocationFromCoordinateStateElement = "<state>";
 	public static String LocationFromCoordinateCityElementEnd = "</city>";
+	public static String LocationFromCoordinateStateElementEnd = "</state>";	
+	public static String LocationFromCoordinateGetURL = "http://where.yahooapis.com/geocode?q=";	
+	
+	/** 
+	 * Static properties Required for communicating with TCP server to retrieve JOTD.
+	 */
+	public static char Space = ' ';
+	public static int BackslashASCII = 92;
+	public static int JokeServerPort = 8000;
+	public static int GreaterThanSignASCII = 62;
+	public static String JokeStartConstant = "JOTD ";
+	public static String JokeServerAddress = "IMPACT.ASU.EDU";
+	public static String ServerNameEndConstant = " WHO ARE YOU";
+	public static String ServerNameStartConstant = "AA1 HELLO THIS IS ";
+	public static String ClientIdentity = "Shoaib-Ahmed-1205058656|Manikandan-Vijayakumar-1204624377|Michael-Sanchez-1000899816";
 }
--- Assignment1/src/processor/RequestProcessor.java
+++ Assignment1/src/processor/RequestProcessor.java
@@ -9,14 +9,187 @@ import commons.*;
  * @author Shoaib
  *
  */
+
 public class RequestProcessor {
 
 	/** 
-	 * Get Location in format "City, State" on the basis of provided IP.	 
+	 * Get Joke Of The Day From TCP Based JOTD Server.	 
 	 */
-	public static String getJoke()
+	public static String getJoke ()
 	{
-		return null;
+		String joke = null;
+		try {
+	          Socket jokeClientsocket = new Socket(ProcessorProperties.JokeServerAddress, ProcessorProperties.JokeServerPort);
+	          BufferedReader socketReader = new BufferedReader(new
+	             InputStreamReader(jokeClientsocket.getInputStream()));
+	          
+	          PrintWriter socketWriter = new PrintWriter(jokeClientsocket.getOutputStream(), 
+	                  true);
+	          
+	          String Servername = "";
+	          boolean finished = false;
+	          
+	          do {	          
+		          String serverMessage = "";
+		          int asciiValue = 0;
+		          while (!socketReader.ready()) {}
+		          do {
+		        	  asciiValue = socketReader.read();
+		        	  if (asciiValue != 0)
+		        		  serverMessage += Character.toString ((char) asciiValue);
+		          } while (!MessageEnd(serverMessage));
+		          
+		          String messageType = GetMessageType(serverMessage);
+		        
+		          String clientMessage = "";
+		          
+		          if (messageType != null & messageType.length() > 0)
+		          {
+			          switch (ProcessorProperties.MessageType.GetMessageType(messageType)) {
+			            case HELLO:  {
+			            	Servername = GetServerName(serverMessage);
+			            	clientMessage = "<AA1 HELLO THIS IS " + ProcessorProperties.ClientIdentity + ">";;		            
+			                     break;
+			            }
+			            case HI:  clientMessage = "<AA1 REQUEST " + Servername + " JOTD>";
+	                    		 break;
+			            case GOODBYE:  {
+			            	finished = true;
+			                break;
+			            }
+			            case REQUEST:
+			                     break;
+			            case RESPONSE:  {
+			            	joke = ExtractJoke(serverMessage);
+			            	clientMessage = "<AA1 GOODBYE " + Servername + ">";
+			            	break;
+			            }			                     
+			            default:  {
+			            	clientMessage = "<AA1 You Sent Invalid Message .....";
+			            	finished = true;
+			                break;
+			            }
+			          }		          		          		          
+		          }
+		          
+		          if (clientMessage.length() > 0)
+		          {
+		        	  socketWriter.write(clientMessage);
+		        	  socketWriter.flush();
+		          }		
+		          else
+		          {
+		        	  finished = true;
+		          }		      
+	          }while (!finished);
+	          
+	          socketWriter.close();
+	          socketReader.close();
+	          jokeClientsocket.close();	          
+	       }
+	       catch(Exception e) {
+	    	   joke = null;
+	       }       
+		
+		return joke;
+	}
+	
+	/** 
+	 * Determine if the end of message has been received or not.	
+	 * @param String representing the current message. 
+	 */
+	public static boolean MessageEnd(String serverMessage)
+	{
+		boolean status = false;
+		try
+		{									
+			if (serverMessage.charAt(serverMessage.length() - 1) == ProcessorProperties.GreaterThanSignASCII )
+			{
+				int PrecedingBackslahes = 0;
+				int iterator = 2;
+				char temp = 0;
+				do
+				{
+					temp = serverMessage.charAt(serverMessage.length()- iterator);
+					if (temp == ProcessorProperties.BackslashASCII)
+					{
+						PrecedingBackslahes++;
+					}
+					iterator++;
+				}while (temp == ProcessorProperties.BackslashASCII); 				
+				
+				if ((PrecedingBackslahes % 2) == 0)
+				{
+					return true;
+				}				
+			}
+		}
+		catch (Exception ex)
+		{
+			status = false;
+		}
+		
+		return status;
+	}
+	
+	/** 
+	 * Extract Message Type from the current completed message.	
+	 * @param String representing the current message. 
+	 */
+	public static String GetMessageType(String serverMessage)
+	{
+		try
+		{
+		String MessageType = serverMessage.substring
+				(serverMessage.indexOf(ProcessorProperties.Space) + 1
+						, serverMessage.indexOf(ProcessorProperties.Space) + 1 + 
+						serverMessage.substring(serverMessage.indexOf(ProcessorProperties.Space) + 1).indexOf(ProcessorProperties.Space)).toUpperCase();
+		return MessageType;
+		}
+		catch (Exception ex)
+		{
+			return null;
+		}
+	}
+
+	/** 
+	 * Extract Server Name from the Initial Message Received from TCP server on establishing connection.	
+	 * @param String representing the current message. 
+	 */
+	public static String GetServerName(String serverMessage)
+	{
+		try
+		{
+			String ServerName = serverMessage.substring
+					(serverMessage.indexOf(ProcessorProperties.ServerNameStartConstant) + ProcessorProperties.ServerNameStartConstant.length()
+							, serverMessage.indexOf(ProcessorProperties.ServerNameStartConstant) + 
+							serverMessage.substring(serverMessage.indexOf(ProcessorProperties.ServerNameStartConstant)).indexOf(ProcessorProperties.ServerNameEndConstant));
+			return ServerName;
+		}
+		catch (Exception ex)
+		{
+			return null;
+		}
+	}
+
+	/** 
+	 * Extract JOTD from the current completed message.	
+	 * @param String representing the current message. 
+	 */
+	public static String ExtractJoke(String serverMessage)
+	{
+		try
+		{
+			String Joke = serverMessage.substring
+					(serverMessage.indexOf(ProcessorProperties.JokeStartConstant) + ProcessorProperties.JokeStartConstant.length()
+							, serverMessage.indexOf(ProcessorProperties.JokeStartConstant) + 
+							(serverMessage.substring(serverMessage.indexOf(ProcessorProperties.JokeStartConstant)).length() - 1 ));
+			return Joke;
+		}
+		catch (Exception ex)
+		{
+			return null;
+		}
 	}
 	
 	/** 
@@ -32,7 +205,7 @@ public class RequestProcessor {
 	        {
 	        	String Data = DoHTTPGet(ProcessorProperties.LocationFromIPGetURL +  ipaddressofdevice);
 	        	
-		        if (Data != null) 
+		        if (Data != null & Data.length() > 0) 
 		        {	        	
 		        	Location = 	Data.substring(
 		        				Data.indexOf(ProcessorProperties.LocationFromIPCityElement), Data.length()).
@@ -70,7 +243,7 @@ public class RequestProcessor {
 				
 		        if (Data != null)
 		        {		        	
-			        if (Data != null & !Data.isEmpty()) 
+			        if (Data.length() > 0) 
 			        {	        	
 			        	Location = 	Data.substring(
 			        				Data.indexOf(ProcessorProperties.LocationFromCoordinateCityElement), Data.length()).
